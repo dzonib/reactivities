ff61c6fe39088aad0ec478c4d2e8a0115181ab24
Nikola <nikola@nursa.com>
2023-02-18 10:51:50 +0100

    feat: added cors (allow for any origin)

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("*");
+    })
+})
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
